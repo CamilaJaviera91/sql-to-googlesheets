ad7561cfa654a0e36f2890fa540189f82c4bd378
Camila Muñoz <cjmunozn@hotmail.com>
2024-11-27 22:43:56 -0300

    feat(spread_id): create spreadsheet ID (you can find it in the URL of your Google Sheet example: https://docs.google.com/spreadsheets/d/1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz/edit#gid=0)

---
+++ id_spreadsheets.py
@@ -0,0 +1,6 @@
+#spreadsheet ID (you can find it in the URL of your Google Sheet example: https://docs.google.com/spreadsheets/d/1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz/edit#gid=0)
+#"1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz" this is the id that we need
+def spreadsheets():
+    SPREADSHEET_ID = '1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz'
+
+    return SPREADSHEET_ID
